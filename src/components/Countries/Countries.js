cd0e4c269f936e96ac7b9b828d601596cfc471cb
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-09-24 02:14:58 +0600

    build done

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -13,13 +14,16 @@ const Countries = () => {
     return (
         <div>
             <h1>Available Countries: {countries.length}</h1>
+            <div  className='countries-container'>
             {
                 countries.map(country => 
                     <Country 
-                    country={country}>
+                    country={country}
+                    key={country.cca3}>
                     </Country>
                 )
             }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {area, region, population,name} = props.country;
+    const {area, region, population,name, flags} = props.country;
     return (
         <div className='country'>
+            <img className='flag' src={flags.png} alt="" />
             <h2 className='country-title'>Country Name: {name.common}</h2>
             <h4>Population: {population}</h4>
             <p><small>Area: {area}</small></p>
